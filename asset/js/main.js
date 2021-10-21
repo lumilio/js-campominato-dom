8435d45f6640e4b51a10fdaca106f314f419c787
lumilio <logistic-division@lumilium.com>
2021-10-21 15:50:38 +0200

    brainstorming...

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,24 @@
 
+
+/* Consegna
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su ogni cella:
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando
+il giocatore clicca su una bomba
+o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. */
+
+
+
+
+
 // seleziono elemento dalla DOM
 const griglia = document.getElementById('container')
 
@@ -8,34 +28,61 @@ while (hardness != '1' && hardness != '2' && hardness != '3') {
     hardness = prompt("scegli un livello di difficoltà : 1 , 2 o 3?");
 }
 
-
 // assegno il valore di ogni opzione
-let rangeMax;
 let cellNumber;
 let cellDimension;
 
 if (hardness == 1){
-    rangeMax = 100;
-    cellNumber = 64;
-    cellDimension = '125px';
-}else if (hardness == 2){
-    rangeMax = 81;
     cellNumber = 100;
-    cellDimension = '100px';
+    cellDimension = '63px';
+}else if (hardness == 2){
+    cellNumber = 81;
+    cellDimension = '70px';
 }else if (hardness == 3){
-    rangeMax = 49;
-    cellNumber = 400;
-    cellDimension = '50px';
+    cellNumber = 49;
+    cellDimension = '90px';
 }
+
+
+
+// funzione utility per generare un numero random compreso in un intervallo
+function random_Utility(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+/* const bombs = []
+
+while (bombs.length < 16) {
+    const numeroBomba = getRandomNumber(1, cellNumber)
+    if (!bombs.includes(numeroBomba)) {
+        bombs.push(numeroBomba)
+    }
+} 
+
+
+console.log(bombs);  */
+
+
+
+
+
+
+
+
+
+
+
 
 
 //creo una funzione che genera la griglia
 function xxx() {
-    for (let i = 0; i < cellNumber; i++) {
+    for (let i = 1; i <= cellNumber; i++) {
 
         //creo il numero casuale dei ivelli
-        let range = Math.floor(Math.random() * rangeMax) + 1 
-
+        let range = i
         //creo un div ripetuto e ci aggiungo una classe
         let cell = document.createElement('div');
         cell.style.width = cellDimension
@@ -46,9 +93,9 @@ function xxx() {
         //creo un secondo div per ogni cella e ci metto il numero 
         let theNumber = document.createElement('div');
         theNumber.innerHTML = range 
-        theNumber.className = 'd-none'
+        theNumber.className = /* 'd-none' */ 
         cell.append(theNumber)  
-
+ 
         //al click modifico le classi dei div
         cell.addEventListener('click',  function () {    
             if (this.classList.contains("color")) {
@@ -59,6 +106,7 @@ function xxx() {
                 theNumber.classList.remove('d-none')
             }
         })
+        
     }
 }
 
